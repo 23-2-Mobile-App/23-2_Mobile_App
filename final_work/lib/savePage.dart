930bae344fa91fa643418c78a5749fa4c44afa67
Youngkwan-Cho <dudrhks1009@naver.com>
2023-11-21 17:00:33 +0900

    Update savePage.dart

--- final_work/lib/savePage.dart
+++ final_work/lib/savePage.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'model/model_record.dart';
 
 class SavePage extends StatefulWidget {
   const SavePage({Key? key}) : super(key: key);
@@ -9,44 +11,36 @@ class SavePage extends StatefulWidget {
 }
 
 class _SavePageState extends State<SavePage> {
-  int count = 5;
-  late Timer timer;
-
-  @override
-  void initState() {
-    super.initState();
-
-  }
-
   @override
   Widget build(BuildContext context) {
+    RecordProvider recordProvider = Provider.of<RecordProvider>(context, listen: false);
     return Scaffold(
       backgroundColor: Color(0xFF01C1FD), // 배경 색상 설정
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-        Container(
-          width: 400,
-        height: 300,
-        decoration: BoxDecoration(
-        color: Colors.green.withOpacity(0.7),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-        ),
-            SizedBox(height: 200,),
-            ElevatedButton(onPressed: (){
-              Navigator.pop(context);
-            }, child: Text("save")),
+            Container(
+              width: 400,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.green.withOpacity(0.7),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                recordProvider.createRecord(distance: 1, pace: 1, time: 1);
+                Navigator.pop(context);
+              },
+              child: Text("save"),
+            ),
           ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
   }
 }
