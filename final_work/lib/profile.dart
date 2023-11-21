a5a27ed6af00abc42fe1a08c412f21173c11e759
Youngkwan-Cho <dudrhks1009@naver.com>
2023-11-21 17:00:31 +0900

    Update profile.dart

--- final_work/lib/profile.dart
+++ final_work/lib/profile.dart
@@ -2,7 +2,6 @@ import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'model/model_auth.dart';
-
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
@@ -11,12 +10,15 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  int _currentIndex =3;
+  int _currentIndex = 3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF01C1FD), // Set the background color here
       appBar: AppBar(
         title: Text('Profile'),
+        backgroundColor: Color(0xFF01C1FD),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.exit_to_app),
@@ -28,59 +30,70 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Consumer<FirebaseAuthProvider>(
-              builder: (context, authProvider, _) {
-                if (authProvider.currentUser == null) {
-                  return CircularProgressIndicator();
-                }
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Consumer<FirebaseAuthProvider>(
+                builder: (context, authProvider, _) {
 
-                if (authProvider.currentUser?.isAnonymous ?? false) {
-                  // Display information for anonymous user
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.network(
-                        'http://handong.edu/site/handong/res/img/logo.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Text('UID: ${authProvider.currentUser?.uid ?? "Anonymous"}'),
-                      Text('Email: Anonymous'),
-                    ],
-                  );
-                } else {
-                  // Display information for authenticated user
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundImage: NetworkImage(
-                          authProvider.currentUser?.photoURL ?? '',
+                  if (authProvider.currentUser == null) {
+                    return Center(
+                        child: Text(
+                          'Loading...',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text('UID: ${authProvider.currentUser?.uid ?? ""}'),
-                      Text('Email: ${authProvider.currentUser?.email ?? ""}'),
+                      );
+                  }
 
-                    ],
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-            const Text('SeokWon Kim', style: TextStyle(fontSize: 14)),
-            const SizedBox(height: 20),
 
-            const Text(
-              'I promise to take the test honestly before GOD',
-              style: TextStyle(fontSize: 14),
-            ),
-          ],
+                  if (authProvider.currentUser?.isAnonymous ?? false) {
+                    // Display information for anonymous user
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          'http://handong.edu/site/handong/res/img/logo.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text('UID: ${authProvider.currentUser?.uid ?? "Anonymous"}'),
+                        Text('Email: Anonymous'),
+                      ],
+                    );
+                  } else {
+                    // Display information for authenticated user
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                            authProvider.currentUser?.photoURL ?? '',
+                          ),
+                        ),
+                        Text('UID: ${authProvider.currentUser?.uid ?? ""}'),
+                        Text('Email: ${authProvider.currentUser?.email ?? ""}'),
+                      ],
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text('SeokWon Kim', style: TextStyle(fontSize: 14)),
+              const SizedBox(height: 20),
+              const Text(
+                'I promise to take the test honestly before GOD',
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: ConvexAppBar(
