6fd689acfc5d5a0c45ce3adb7382f4c2dca86068
Youngkwan-Cho <dudrhks1009@naver.com>
2023-12-01 17:42:39 +0900

    Update app.dart

--- final_work/lib/app.dart
+++ final_work/lib/app.dart
@@ -1,17 +1,3 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 import 'package:shrine/detail.dart';
 import 'package:shrine/add.dart';
@@ -28,7 +14,6 @@ import 'login.dart';
 import 'map.dart';
 import 'home.dart';
 
-// TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
   const ShrineApp({Key? key}) : super(key: key);
 
@@ -48,11 +33,7 @@ class ShrineApp extends StatelessWidget {
         '/goalPage' : (BuildContext context) => const GoalPage(),
         '/edit' : (BuildContext context) => const ProfileEditPage(),
       },
-      // TODO: Customize the theme (103)
       theme: ThemeData.light(useMaterial3: true),
     );
   }
 }
-
-// TODO: Build a Shrine Theme (103)
-// TODO: Build a Shrine Text Theme (103)
