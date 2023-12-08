d4e880f570e5b9dae7f48552ee41d0981bb5113f
Youngkwan-Cho <dudrhks1009@naver.com>
2023-12-08 16:36:10 +0900

    Update runPage.dart

--- final_work/lib/runPage.dart
+++ final_work/lib/runPage.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:core';
-import 'dart:typed_data';
-import 'dart:ui' as dui;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
