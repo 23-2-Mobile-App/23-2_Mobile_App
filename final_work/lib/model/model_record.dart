76fbf4e54f9ee3a6076963db631f0b56b9c1706b
Youngkwan-Cho <dudrhks1009@naver.com>
2023-12-08 16:36:15 +0900

    Update model_record.dart

--- final_work/lib/model/model_record.dart
+++ final_work/lib/model/model_record.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
-
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:flutter/cupertino.dart';
 import 'record.dart';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RecordProvider with ChangeNotifier {
